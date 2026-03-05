669d0ed384d482a3d45bfc0529dd5657a6f16d85
psbwb <eelyushabwb@icloud.com>
2026-03-05 22:11:42 +0700

    add async engine
    -done: bd connection only

---
+++ src/database.py
@@ -0,0 +1,6 @@
+import asyncio
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy import text
+from src.config import settings
+
+engine = create_async_engine(settings.DB_URL)
--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 
 sys.path.append(str(Path(__file__).parent.parent))
 from src.api.hotels import router as router_hotels
+from src.config import settings
+from src.database import *
 
 app = FastAPI()
 app.include_router(router_hotels)
