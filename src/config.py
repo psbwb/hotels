423a55fabea20a3537eb19088fc8bbae92d1b86e
psbwb <eelyushabwb@icloud.com>
2026-03-07 17:10:50 +0700

    first db migration
    - added hotels model
    - added session_maker
    - made init migration (first table is in DB)

--- src/config.py
+++ src/config.py
@@ -11,6 +11,6 @@ class Settings(BaseSettings):
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
--- src/database.py
+++ src/database.py
@@ -1,6 +1,12 @@
-import asyncio
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
+
 from src.config import settings
 
 engine = create_async_engine(settings.DB_URL)
+
+async_sessionmaker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
